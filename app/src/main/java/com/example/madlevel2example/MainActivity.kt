bb137e26063f57237f67117377e1b32c411d9f3a
Sven van Putten <svenvanputten@gmail.com>
2020-09-07 09:52:30 +0200

    Implemented the initViews method in the MainActivity

--- app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.madlevel2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.adapters.ReminderAdapter
 import com.example.madlevel2example.classes.Reminder
 import com.example.madlevel2example.databinding.ActivityMainBinding
@@ -11,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private val reminders = arrayListOf<Reminder>()
     private val reminderAdapter = ReminderAdapter(reminders)
 
-    // Don't forget to create a binding object as you did in previous assignments.
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +24,26 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
+    /**
+     * Method for setting up views and event handlers
+     */
     private fun initViews() {
-        TODO("Not yet implemented")
+        // Create an onClickListener associated with the button
+        binding.btnAddReminder.setOnClickListener {
+            val reminder = binding.etReminder.text.toString()
+            addReminder(reminder)
+        }
+
+        // Initialize the recycler view with a linear layout manager, adapter
+        binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
     }
 
+    /**
+     * Method for adding reminders
+     */
+    private fun addReminder(reminder: String) {
+        TODO("Not yet implemented")
+    }
 }
