f4d6e8f228a807cfeb304d1c642792a120574226
Sven van Putten <svenvanputten@gmail.com>
2020-09-07 10:17:40 +0200

    Added the createItemTouchHelper method for adding swipe events to reminders

--- app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.madlevel2example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.adapters.ReminderAdapter
@@ -23,22 +24,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initViews()
-    }
 
-    /**
-     * Method for setting up views and event handlers
-     */
-    private fun initViews() {
         // Create an onClickListener associated with the button
         binding.btnAddReminder.setOnClickListener {
             val reminder = binding.etReminder.text.toString()
             addReminder(reminder)
         }
+    }
 
+    /**
+     * Method for setting up the views for the application
+     */
+    private fun initViews() {
         // Initialize the recycler view with a linear layout manager, adapter
         binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         binding.rvReminders.adapter = reminderAdapter
         binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(binding.rvReminders)
     }
 
     /**
@@ -52,5 +55,36 @@ class MainActivity : AppCompatActivity() {
         } else {
             Snackbar.make(binding.etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    /**
+     * Create a touch helper to recognize when a user swipes an item from a recycler view.
+     * An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+     * and uses callbacks to signal when a user is performing these actions.
+     */
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+
+        return ItemTouchHelper(callback)
     }
 }
